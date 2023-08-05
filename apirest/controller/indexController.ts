cd4fb3864de986d142a7f4a6bbfe3d99e7c05ed3
marcelolav <marcelo.lavandeira@gmail.com>
2023-08-04 22:51:09 -0300

    Se elimina el API se construye aparte el api para la app.

--- apirest/controller/indexController.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-
-class IndexController {
-     public index(req: Request, res: Response) {
-          res.send("<h1>La API Funciona correctamente.</h1>");
-     }
-}
-
-export const indexController = new IndexController();
